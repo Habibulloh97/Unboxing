cf68b29f1ff0fe3756adb44f9c2ff9b9039d7cd0
Habibulloh97 <81605677+Habibulloh97@users.noreply.github.com>
2021-06-08 16:48:15 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> unbox = new List<object>();
+            unbox.Add(7);
+            unbox.Add(28);
+            unbox.Add(-1);
+            unbox.Add(true);
+            unbox.Add("chair");
+            int count=0;
+            for (var idx = 0; idx < unbox.Count; idx++)
+            {
+                if (unbox[idx] is int)
+                {
+                    count+=(int)unbox[idx];
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
